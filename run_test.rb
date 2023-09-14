197842bc889393d058b2ac5b1148ddc8078f750f
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2023-09-14 09:55:05 -0400

    test on more modern mruby, with more debug tools enabled

--- run_test.rb
+++ run_test.rb
@@ -9,24 +9,29 @@ if __FILE__ == $0
 
   Dir.mkdir 'tmp'  unless File.exist?('tmp')
   unless File.exist?(dir)
-    system "git clone #{repository} --branch 2.1.2 #{dir}"
+    system "git clone #{repository} --branch 3.2.0 #{dir}"
   end
 
   exit system(%Q[cd #{dir}; MRUBY_CONFIG=#{File.expand_path __FILE__} ruby minirake #{build_args.join(' ')}])
 end
 
 MRuby::Build.new do |conf|
-  toolchain :gcc
+  toolchain :clang
+  enable_sanitizer "address,undefined"
+  enable_debug
+  enable_test # IMPORTANT!
+  disable_lock
+  conf.cc.flags.delete '-fstack-protector-strong'
+  conf.cc.flags += %w( -fpic -fstack-protector-all )
+
   conf.gembox 'default'
 
   conf.gem core: 'mruby-io'
   conf.gem core: 'mruby-pack'
   conf.gem core: 'mruby-socket'
-  conf.gem :git => 'git@github.com:iij/mruby-mtest.git'
+  conf.gem mgem: 'mruby-mtest'
 
   conf.gem File.expand_path(File.dirname(__FILE__)) do |c|
     c.add_test_dependency 'mruby-mtest'
   end
-
-  conf.enable_test
 end
